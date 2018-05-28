042e29e3ca539eda55a40f76c07f4bf5bc411e69
Denise Pendleton <dpendl1@hotmail.com>
2018-05-28 00:55:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+driver = [
+  {name: "bob", revenue: 12.2},
+  {name: "jan", revenue: 205}
+]
+
+function driversWithRevenueOver(driver, revenue){
+let revDrivers =   driver.filter(function(drive){return drive.revenue > revenue})
+return revDrivers
+}
+
+function driverNamesWithRevenueOver(driver, revenue){
+  let revDrivers =   driver.filter(function(drive){return drive.revenue > revenue})
+  return driveNames =  revDrivers.map(function(drive){ return drive.name})
+}
+
+// let varObj = {name: "bob"}
+function exactMatch(driver, {name: b}){
+let exact = driver.filter(function(drive){
+  return drive.name == b })
+return exact
+}
